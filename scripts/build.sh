954f49fc6464b302950c6d29a0a8333335739938
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-11-06 00:17:06 +0800

    chore: compress the binaries within 25MB to stay within the limit of github

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,20 +9,23 @@ echo "Building $name $version"
 
 npm run build
 
-mkdir -p build
+rm -rf build
+mkdir build
 
 function build_for_platform {
 	platform="$1"
 	target="bun-$platform-x64-baseline"
 	outfile="build/$name-$version-$platform"
 	bun build dist/cli.js --compile --target="$target" --outfile "$outfile"
-	if [ -f "$outfile.exe" ]; then
-		zip -r "$outfile.zip" "$outfile.exe"
-	else
-		zip -r "$outfile.zip" "$outfile"
-	fi
 }
+
+set -v
 
 build_for_platform "linux"
 build_for_platform "macos"
 build_for_platform "windows"
+
+cd build
+xz -k -T0 "$name-$version-linux"
+zip -r -9 "$name-$version-macos.zip" "$name-$version-macos"
+7z a -mx9 "$name-$version-windows.7z" "$name-$version-windows.exe"
